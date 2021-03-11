e5354bafb0b99d8bccc423c463dcff8516845a8c
siddharth199829 <79243535+siddharth199829@users.noreply.github.com>
2021-03-10 20:57:45 -0500

    Update Handshake.java

--- src/gatorShare/Handshake.java
+++ src/gatorShare/Handshake.java
@@ -8,9 +8,17 @@ public class Handshake {
 
     private int ID;
     private Socket socket = null;
+    private String header;
 
     public Handshake(String message, int myID) {
         System.out.println(message.substring(28));
+        System.out.println(myID);
+    }
+    
+    public Handshake(String message, int myID, String header) {
+    	this.ID = myID;
+    	this.header = header;
+    	System.out.println(message.substring(28));
         System.out.println(myID);
     }
 }
